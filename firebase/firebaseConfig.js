2597dde99a212efa56e66e5aaddd753bebeb69c1
André Pereira <andrepereirasa100@gmail.com>
2025-05-04 22:48:21 -0300

    agr vai

--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -1,20 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
-import 'dotenv/config'; 
+import { API_KEY, APP_ID, MEASUREMENT_ID, MESSAGING_SENDER_ID } from '@env';
 
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
+  apiKey: API_KEY,
   authDomain: "medit8-app.firebaseapp.com",
   projectId: "medit8-app",
   storageBucket: "medit8-app.firebasestorage.app",
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID
 };
 
 // Initialize Firebase
